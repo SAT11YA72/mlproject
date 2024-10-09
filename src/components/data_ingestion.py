bf6685942f7354080c4c9849665fbe03fc7e1b52
SAT11YA72 <gsp883337@gmail.com>
2024-10-09 19:29:16 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -52,3 +55,6 @@ if __name__=="__main__":
 
     data_tranformation = DataTransformation()
     train_arr,test_arr,preprocessor_obj_file_path = data_tranformation.initiate_data_transformation(train_data,test_data)
+   
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
